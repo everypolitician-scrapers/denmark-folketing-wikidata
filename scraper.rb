128a36f6feb34fbdefdf2485a7b13f694529b781
Chris Mytton <chrismytton@gmail.com>
2015-10-22 16:40:14 +0100

    Initial scraper

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,33 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'wikidata/fetcher'
+require 'nokogiri'
+require 'open-uri/cached'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+OpenURI::Cache.cache_path = '.cache'
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+def noko_for(url)
+  Nokogiri::HTML(open(URI.escape(URI.unescape(url))).read) 
+end
+
+def wikinames_from(url)
+  noko = noko_for(url)
+  names = noko.xpath('//h2/following-sibling::ul//a[contains(@href, "/wiki/") and not(@class="new")]/@title').map(&:text)
+  abort "No names" if names.count.zero?
+  names
+end
+
+def fetch_info(names)
+  WikiData.ids_from_pages('en', names).each do |name, id|
+    data = WikiData::Fetcher.new(id: id).data('en', 'fr') rescue nil
+    unless data
+      warn "No data for #{p}"
+      next
+    end
+    data[:original_wikiname] = name
+    ScraperWiki.save_sqlite([:id], data)
+  end
+end
+
+names = wikinames_from('https://da.wikipedia.org/wiki/Folketingsmedlemmer_valgt_i_2015')
+
+fetch_info names.uniq
