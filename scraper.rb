20bbf2709e347b61b483a5df85131b3dcfce6ce2
Tony Bowden <tony@tmtm.com>
2016-03-25 20:07:17 +0000

    Add historic people

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,8 @@
-require 'bundler'
-Bundler.require
+require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath(
+names = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://da.wikipedia.org/wiki/Folketingsmedlemmer_valgt_i_2015',
   xpath: '//h2/following-sibling::ul//a[contains(@href, "/wiki/") and not(@class="new")]/@title',
-)
+) | EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/denmark-folketing-wp', column: 'wikiname')
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { da: names })
-warn EveryPolitician::Wikidata.notify_rebuilder
