e9a918ce362b7f87bd276ea7cc8b12f9c77d6204
Tony Bowden <tony@tmtm.com>
2016-06-02 20:37:40 +0200

    also fetch by category

--- scraper.rb
+++ scraper.rb
@@ -1,8 +1,10 @@
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath( 
+wp = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://da.wikipedia.org/wiki/Folketingsmedlemmer_valgt_i_2015',
   xpath: '//h2/following-sibling::ul//a[contains(@href, "/wiki/") and not(@class="new")]/@title',
-) | EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/denmark-folketing-wp', column: 'wikiname')
+) 
+scraped = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/denmark-folketing-wp', column: 'wikiname')
+category = WikiData::Category.new( "Kategori:Folketingsmedlemmer i 2010'erne", 'da').member_titles 
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { da: names })
+EveryPolitician::Wikidata.scrape_wikidata(names: { da: wp | scraped | category })
