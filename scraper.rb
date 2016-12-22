1489b30c06a09c0c0c6164338b5e7dad8cb245d5
Tony Bowden <tony@tmtm.com>
2016-12-22 08:12:53 +0000

    Also scrape 2011 Members

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,16 @@
 require 'wikidata/fetcher'
 
-wp = EveryPolitician::Wikidata.wikipedia_xpath( 
+wp2015 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://da.wikipedia.org/wiki/Folketingsmedlemmer_valgt_i_2015',
   xpath: '//h2/following-sibling::ul//a[contains(@href, "/wiki/") and not(@class="new")]/@title',
-) 
-scraped = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/denmark-folketing-wp', column: 'wikiname')
-category = WikiData::Category.new( "Kategori:Folketingsmedlemmer i 2010'erne", 'da').member_titles 
+)
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { da: wp | scraped | category })
+wp2011 = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://da.wikipedia.org/wiki/Folketingsmedlemmer_valgt_i_2011',
+  xpath: '//h2/following-sibling::ul//a[contains(@href, "/wiki/") and not(@class="new")]/@title',
+)
+
+scraped = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/denmark-folketing-wp', column: 'wikiname')
+category = WikiData::Category.new( "Kategori:Folketingsmedlemmer i 2010'erne", 'da').member_titles
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { da: wp2015 | wp2011 | scraped | category })
