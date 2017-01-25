8b942fa09092f2a6b11f9f3e57dc929be43dd7e3
Tony Bowden <tony@tmtm.com>
2017-01-25 20:02:56 +0000

    Add some members by ID

--- scraper.rb
+++ scraper.rb
@@ -10,7 +10,9 @@ wp2011 = EveryPolitician::Wikidata.wikipedia_xpath(
   xpath: '//h2/following-sibling::ul//a[contains(@href, "/wiki/") and not(@class="new")]/@title',
 )
 
+by_id = %w(Q28167615 Q2037525 Q22956366 Q16167420 Q28528045 Q23311251 Q331736)
+
 scraped = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/denmark-folketing-wp', column: 'wikiname')
 category = WikiData::Category.new( "Kategori:Folketingsmedlemmer i 2010'erne", 'da').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { da: wp2015 | wp2011 | scraped | category })
+EveryPolitician::Wikidata.scrape_wikidata(ids: by_id, names: { da: wp2015 | wp2011 | scraped | category })
